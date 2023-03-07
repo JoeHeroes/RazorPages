3eb47118f398f4cb07770c646af82bfe80d58f94
Krzysztof Góral <k.goral@topmotive.eu>
2023-03-07 19:00:06 +0100

    Dodaj pliki projektów.

---
+++ xxx/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml.Serialization;
+
+namespace xxx.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+
+        public IActionResult onPost()
+        {
+            string _firstname = Request.Form["imie"];
+            string _secondname = Request.Form["nazwisko"];
+
+            return RedirectToPage("welcome",new { firstname = _firstname , secondname = _secondname});
+
+        }
+    }
+}
